071088313a44c7b7e4b19b31a9ff09a94f9e5d7a
patrickodev <50785856+patrickodev@users.noreply.github.com>
2023-04-18 21:18:56 -0300

    Botao do lixo

--- src/ShoppingCart.tsx
+++ src/ShoppingCart.tsx
@@ -36,6 +36,16 @@ const ShoppingCart: React.FC<Props> = ({ items }) => {
     setCartItems(updatedCartItems);
   };
 
+  const handleRemoveAll = (itemId: number) => {
+    const updatedCartItems = cartItems.map(item => {
+      if (item.id === itemId) {
+        return { ...item, quantity: Math.max(0, item.quantity - item.quantity) }; // Atualiza a quantidade do item no carrinho, com mínimo de 0
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
+  };
+
   const calculateSubtotal = (item: CartItem) => {
     return item.price * item.quantity;
   };
@@ -45,29 +55,33 @@ const ShoppingCart: React.FC<Props> = ({ items }) => {
   };
 
   return (
-    <div>
-      {cartItems.length === 0 ? (
-        <p>O carrinho de compras está vazio</p>
-      ) : (
-        <ul>
-        <h1>Carrinho de compras</h1>
-          {cartItems.map(item => (
-            <li key={item.id}>
-            <img src={item.image} alt={item.name} />
-              <div>
-              <p> {item.description} </p>
-              <p> R${item.price} </p>
-              <p> Quantidade: {item.quantity}</p>
-              <button onClick={() => handleAddToCart(item.id)}  > + </button>
-              <button onClick={() => handleRemoveFromCart(item.id)}  > -  </button>
-              {item.quantity} x R${item.price.toFixed(2)} = R${calculateSubtotal(item).toFixed(2)}
-              </div>
-            </li>
-          ))}
-          Total: R${calculateTotal().toFixed(2)}
-        </ul>
-      )}
-    </div>
+    <div className="shopping-cart">
+  {cartItems.length === 0 ? (
+    <p className="cart-empty">O carrinho de compras está vazio</p>
+  ) : (
+    <ul className="cart-items">
+      <h1 className="cart-header">Carrinho de compras</h1>
+      {cartItems.map(item => (
+        <li key={item.id} className="cart-item">
+          <img src={item.image} alt={item.name} className="cart-item-image" />
+          <div className="cart-item-details">
+            <p className="cart-item-description">{item.description}</p>
+            <p className="cart-item-price">R${item.price}</p>
+            <p className="cart-item-quantity">Quantidade: {item.quantity}</p>
+            <button onClick={() => handleAddToCart(item.id)} className="cart-item-button">+</button>
+            <button onClick={() => handleRemoveFromCart(item.id)} className="cart-item-button">-</button>
+            <p className="cart-item-subtotal">{item.quantity} x R${item.price.toFixed(2)} = R${calculateSubtotal(item).toFixed(2)}</p>
+            <button onClick={() => handleRemoveAll(item.id)} className="cart-item-button">LIXO</button>
+          </div>
+        </li>
+      ))}
+      <p className="cart-total">
+        <p className="cart-total-label">Total:</p>
+        <p className="cart-total-value">R${calculateTotal().toFixed(2)}</p>
+      </p>
+    </ul>
+  )}
+</div>
   );
 };
 
